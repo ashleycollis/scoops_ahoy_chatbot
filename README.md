3dfb1b8e14decdce9e44809bb94c7b7738af1b75
Ashley Collis <ashleycollisburgess@gmail.com>
2020-02-24 20:02:29 -0500

    basic setup

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ This boilerplate makes it easy to make testable front end code. It includes:
 - Open 3 terminals and run:
   - `npm run webpack:dev`
   - `npm run server`
-  - `npm run jest:watch`
+  - `npm run test:watch`
 
 ![diagram](diagram.png)
 
@@ -25,7 +25,3 @@ The `package.json` includes `npm run test` and `npm run webpack:prod`. These com
 
 - `npm run test` will run tests and exit instead of watching for file changes.
 - `npm run webpack:prod` will build a production webpack build of your javascript files. If you have multiple files it'll package them all into one file (dist/main.js). This `dist` folder is gitignored but it should be pushed to the gh-pages branch. See [this guide](https://gist.github.com/cobyism/4730490).
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import sum from './sum';
 import axios from 'axios';
+const Carousel = require('vanilla-js-carousel');
+console.log(sum(1, 2));
 
+var carousel = new Carousel({
+  elem: 'carousel', // id of the carousel container
+  autoplay: true, // starts the rotation automatically
+  infinite: true, // enables infinite mode
+  interval: 1500, // interval between slide changes
+  initial: 0, // slide to start with
+  dots: true, // show navigation dots
+  arrows: true, // show navigation arrows
+  buttons: false, // hide <play>/<stop> buttons,
+  btnStopText: 'Pause', // <stop> button text
+});
 
-console.log(sum(1,2));
+// Show the 3rd slide (Numeration of slides starts at 0)
+carousel.show(2);
+
+// Move to the next slide
+carousel.next();
 
 const main = async () => {
-  const res = await axios.get('https://fizal.me/pokeapi/api/v2/name/bulbasaur.json');
+  const res = await axios.get(
+    'https://fizal.me/pokeapi/api/v2/name/bulbasaur.json'
+  );
   console.log(res.data);
-}
+};
 
 main();
 
+function openForm() {
+  document.getElementById('myForm').style.display = 'block';
+}
 
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+}
